c8e74359345f2e691c4a4f704f12f6835d48ec60
Maxime Ortuno <maxime.ortuno@hotmail.fr>
2023-08-02 22:19:03 +0200

    02/08

---
+++ src/components/Social/index.jsx
@@ -0,0 +1,20 @@
+import classes from './style.module.css'
+
+export default function Social() {
+  return (
+    <section className={classes.section3}>
+        <div className={classes.break}>
+        <hr className={classes.line}/><p>Ou...</p><hr className={classes.line}/>
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" width="274" height="238" viewBox="0 0 274 238" fill="none">
+<path d="M-56.4669 0.580962L71.9739 0.814933C175.878 1.00424 262.698 79.9693 272.711 183.39C283.281 292.564 204.817 390.231 95.9303 403.436L-65.8014 423.049C-155.988 433.987 -242.312 383.114 -276.434 298.918C-306.566 224.567 -289.636 139.421 -233.353 82.2525L-220.339 69.0336C-177.122 25.1376 -118.066 0.468722 -56.4669 0.580962Z" fill="url(#paint0_linear_1_33)"/>
+<defs>
+<linearGradient id="paint0_linear_1_33" x1="-325.28" y1="223.354" x2="288.897" y2="212.969" gradientUnits="userSpaceOnUse">
+<stop stop-color="#2DA1BC" stop-opacity="0.61"/>
+<stop offset="1" stop-color="#DCDCDC" stop-opacity="0"/>
+</linearGradient>
+</defs>
+</svg>
+    </section>
+  );
+}
--- src/pages/LogSign.jsx
+++ src/pages/LogSign.jsx
@@ -2,6 +2,7 @@ import image from "../images/Plan-de-travail-7-copie 1Logo.svg"
 import Banner from "../components/Banner/index.jsx"
 import Greeting from "../components/Greeting/index.jsx"
 import Form from "../components/Form/index.jsx"
+import Social from "../components/Social/index.jsx"
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
     <Banner image={image}/>
     <Greeting/>
     <Form/>
+    <Social/>
       { /* <Header/>
      
       <Housing />
